800589dcaa747192d1f5330bf43f9676f8929a89
Apoliak7777 <167175652+Apoliak7777@users.noreply.github.com>
2024-05-17 22:43:18 +0200

    Create Player.js

---
+++ Player.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const playerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    expiry: { type: Date, required: true }
+});
+
+module.exports = mongoose.model('Player', playerSchema);
